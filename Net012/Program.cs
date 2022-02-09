a517ce181344575d70c1741b8fa25493e63fbe68
RimvydasVainutisCS <97608500+RimvydasVainutisCS@users.noreply.github.com>
2022-02-09 16:31:32 +0200

    the barebones of the square matrix program

--- Net012/Program.cs
+++ Net012/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Net012
 {
@@ -10,6 +6,37 @@ namespace Net012
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("NET01.2 Generics, delegates and events");
+
+            var squareMatrixOneSize = 9;
+            var indexOneOne = new Index(0, 0);
+            var indexOneTwo = new Index(0, 1);
+            var indexOneThree = new Index(0, 2);
+            var indexTwoOne = new Index(1, 0);
+            var indexTwoTwo = new Index(1, 1);
+            var indexTwoThree = new Index(1, 2);
+            var indexThreeOne = new Index(2, 0);
+            var indexThreeTwo = new Index(2, 1);
+            var indexThreeThree = new Index(2, 2);
+
+            var squareMatrixOne = new SquareMatrix(squareMatrixOneSize, 
+                                                   indexOneOne, indexOneTwo, indexOneThree,
+                                                   indexTwoOne, indexTwoTwo, indexTwoThree,
+                                                   indexThreeOne, indexThreeTwo, indexThreeThree);
+
+
+            int[] array = new int[9];
+            array[0] = 11;
+            array[1] = 12;
+            array[2] = 13;
+            array[3] = 21;
+            array[4] = 22;
+            array[5] = 23;
+            array[6] = 31;
+            array[7] = 32;
+            array[8] = 33;
         }
+
+
     }
 }
--- Net012/SquareMatrix.cs
+++ Net012/SquareMatrix.cs
@@ -1,19 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Net012
+﻿namespace Net012
 {
     public class SquareMatrix : BaseMatrix
     {
-        public SquareMatrix(Index indexOne, Index indexTwo)
+        public SquareMatrix(int matrixSize, Index indexOne, Index indexTwo, Index indexThree,
+                            Index indexFour, Index indexFive, Index indexSix,
+                            Index indexSeven, Index indexEight, Index indexNine)
         {
+            baseMatrixSize = matrixSize;
             IJ1 = indexOne;
             IJ2 = indexTwo;
+            IJ3 = indexThree;
+            IJ4 = indexFour;
+            IJ5 = indexFive;
+            IJ6 = indexSix;
+            IJ7 = indexSeven;
+            IJ8 = indexEight;
+            IJ9 = indexNine;
         }
 
-
+        // Make a check if given indexes can create a
+        // square matrix: n x n
     }
 }
